ad74eda5010cd595f51fffaad0a210eb2a212458
Eleksius <eleksius@mail.ru>
2025-01-04 22:04:39 +0300

    another one

--- funcs.py
+++ funcs.py
@@ -5,30 +5,27 @@ import screen_brightness_control as sbc
 
 
 def change_volume(value):
-    # Получаем устройство воспроизведения
     devices = AudioUtilities.GetSpeakers()
     interface = devices.Activate(IAudioEndpointVolume.iid_, CLSCTX_ALL, None)
     volume = cast(interface, POINTER(IAudioEndpointVolume))
 
-    # Получаем текущую громкость
     current_volume = volume.GetMasterVolumeLevelScalar()
 
     if isinstance(value, str):
         if value.startswith('+'):
-            change = float(value[1:]) / 100  # Конвертируем в процент
+            change = float(value[1:]) / 100
         elif value.startswith('-'):
-            change = -float(value[1:]) / 100  # Конвертируем в процент
+            change = -float(value[1:]) / 100
         elif value.isnumeric():
-            change = float(value) / 100  # Устанавливаем громкость как процент
+            change = float(value) / 100
             current_volume = 0
         else:
             raise ValueError("Некорректный формат значения")
     else:
         raise ValueError("Значение должно быть строкой")
 
-    # Устанавливаем или изменяем громкость
     new_volume = current_volume + change
-    new_volume = max(0.0, min(new_volume, 1.0))  # Ограничиваем значение от 0 до 1
+    new_volume = max(0.0, min(new_volume, 1.0))
     volume.SetMasterVolumeLevelScalar(new_volume, None)
 
 # Примеры использования:
@@ -38,24 +35,22 @@ def change_volume(value):
 
 
 def change_brightness(value):
-    # Получение текущей яркости
     current_brightness = int(sbc.get_brightness()[0])
 
     if isinstance(value, str):
         if value.startswith('+'):
-            change = int(value[1:])  # Увеличение яркости
+            change = int(value[1:])
             new_brightness = current_brightness + change
         elif value.startswith('-'):
-            change = int(value[1:])  # Уменьшение яркости
+            change = int(value[1:])
             new_brightness = current_brightness - change
         elif value.isnumeric():
-            new_brightness = int(value)  # Установка яркости
+            new_brightness = int(value)
         else:
             raise ValueError("Некорректный формат значения")
     else:
         raise ValueError("Значение должно быть строкой")
 
-    # Ограничение значения яркости от 0 до 100
     new_brightness = max(0, min(new_brightness, 100))
 
     sbc.set_brightness(new_brightness)
